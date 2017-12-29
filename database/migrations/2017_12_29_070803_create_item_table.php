38412154a19da4a807e765c103ef3fe2690ba886
Dipanjan Kundu <dipsraj.kundu@gmail.com>
2017-12-29 12:53:08 +0530

    Migrations done, TimeZone Changed to Kolkata

--- database/migrations/2017_12_29_070803_create_item_table.php
+++ database/migrations/2017_12_29_070803_create_item_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateItemTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('item', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('room_id');
             $table->string('name');
-            $table->string('username')->unique();
-            $table->string('password');
-            $table->string('role');
             $table->string('status');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('on_off_status');
+            $table->string('output_pin');
+            $table->string('input_pin');
         });
     }
 
@@ -32,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('item');
     }
 }
--- database/migrations/2017_12_29_071413_create_access_log_table.php
+++ database/migrations/2017_12_29_071413_create_access_log_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateAccessLogTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('access_log', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('username')->unique();
-            $table->string('password');
-            $table->string('role');
-            $table->string('status');
-            $table->rememberToken();
+            $table->integer('user_id');
+            $table->integer('item_id');
+            $table->string('item_on_off_status');
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('access_log');
     }
 }
