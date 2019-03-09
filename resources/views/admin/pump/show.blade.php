2bcefc6a8512b9223af5d753cde2b36346d7db83
Monojit Halder <monojit.halder@ingrammicro.com>
2019-03-09 13:10:11 +0530

    Do some changes into new pump module

--- resources/views/admin/pump/show.blade.php
+++ resources/views/admin/pump/show.blade.php
@@ -14,7 +14,7 @@
 
     .fill {
         width: 168px;
-        height: 152px;
+        height: 0px;
         border: 1px solid #000;
         background: green;
         left: 4px;
@@ -26,8 +26,8 @@
 @section('content')
     <div class="container">
         <div>
-            <p>Reserver Water Lavel: <span id="reserver-status">OK</span></p>
-            <p>Pump: <span id="pump-on-status">ON</span></p>
+            <p>Reserver Water Lavel: <span id="reserver-status">...</span></p>
+            <p>Pump: <span id="pump-on-status">...</span></p>
         </div>
         <div id="watertank">
             <div class="fill">
@@ -39,45 +39,65 @@
 @endsection
 @section('custom-script')
     <script>
-        var url = "http://"+"{{ $data['ip'] }}" + "/waterLavel/";
-        $.ajax({
-            url: url, success: function (result) {
-                result = JSON.parse(result);
-                var result_full = 240;
-                var result_percentage = 0;
-                result_percentage = (100 * result.distance) / 240;
+        function fetch_water_level() {
+            var url = "http://"+"{{ $data['ip'] }}" + "/waterLavel";
+            $.ajax({
+                url: url, success: function (result) {
+                    result = result.replace("'", '"');
+                    result = result.replace("'", '"');
+                    result = JSON.parse(result);
+                    var result_full = 100;
+                    var result_percentage = 0;
+                    result_percentage = (100 * result.water_level) / 100;
 
-                var css_full = 152;
-                var css_water_lavel = (152 * result_percentage) / 100;
-                $(".fill").height(css_water_lavel)
-                console.log(result);
-            }
-        });
-        url = "http://"+"{{ $data['ip'] }}" + "/status/type=PUMP";
-        $.ajax({
-            url: url, success: function (result) {
-                result = JSON.parse(result);
-                if(result.pump_on_status == 1) {
-                    $("#pump-on-status").html("ON");
+                    var css_full = 152;
+                    var css_water_lavel = (152 * result_percentage) / 100;
+                    if(css_water_lavel > 152) {
+                        css_water_lavel = 152;
+                    }
+                    $(".fill").height(css_water_lavel)
+                    console.log(result);
                 }
-                else {
-                    $("#pump-on-status").html("OFF");
+            });
+        }
+
+        function fetch_pump_status() {
+            var url = "http://"+"{{ $data['ip'] }}" + "/status?type=PUMP";
+            $.ajax({
+                url: url, success: function (result) {
+                    result = result.replace("'", '"');
+                    result = result.replace("'", '"');
+                    result = JSON.parse(result);
+
+                    if(result.pump_on_status == 1) {
+                        $("#pump-on-status").html("ON");
+                    }
+                    else {
+                        $("#pump-on-status").html("OFF");
+                    }
+                    console.log(result);
                 }
-                console.log(result);
-            }
-        });
-        url = "http://"+"{{ $data['ip'] }}" + "/status/type=RESERVER";
-        $.ajax({
-            url: url, success: function (result) {
-                result = JSON.parse(result);
-                if(result.reserver_status == 0) {
-                    $("#reserver-status").html("OK");
+            });
+        }
+        function fetch_reserver_status() {
+            var url = "http://"+"{{ $data['ip'] }}" + "/status?type=RESERVER";
+            $.ajax({
+                url: url, success: function (result) {
+                    result = result.replace("'", '"');
+                    result = result.replace("'", '"');
+                    result = JSON.parse(result);
+                    if(result.reserver_status == 0) {
+                        $("#reserver-status").html("OK");
+                    }
+                    else {
+                        $("#reserver-status").html("LOW");
+                    }
                 }
-                else {
-                    $("#reserver-status").html("LOW");
-                }
-            }
-        });
+            });
+        }
+        setInterval(fetch_water_level,1000);
+        setInterval(fetch_pump_status,1000);
+        setInterval(fetch_reserver_status,1000);
     </script>
 @endsection
 
