f669e6d5790c3eb25d8352d39bd3d79247d048ca
Monojit Halder <monojit.halder@ingrammicro.com>
2020-01-11 12:06:07 +0530

    Bugfix in pump master controll

--- resources/views/admin/pump/show.blade.php
+++ resources/views/admin/pump/show.blade.php
@@ -116,8 +116,8 @@
         }
         
         function master_control() {
-            $.post( "//pump/master-control", function( data ) {
-
+            $.post( "/pump/master-control", function( data ) {
+                
             });
         }
     </script>
