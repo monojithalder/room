abbda8cba692644d8d3649b627dba2107f8eaa8e
monojit halder <monojit.halder@innoraft.com>
2021-02-24 10:10:50 +0530

    Change pump debug view

--- resources/views/admin/pump/debug.blade.php
+++ resources/views/admin/pump/debug.blade.php
@@ -5,73 +5,46 @@
     <link rel="stylesheet" href="{{asset('css/custom_css/alertmessage.css')}}">
 @endsection
 <style>
-    #watertank{
-        background: url({{ URL::asset('images/tank.png') }}) no-repeat;
-        height: 229px;
-        padding-top: 34px;
-        position: relative;
+    #customers {
+        font-family: Arial, Helvetica, sans-serif;
+        border-collapse: collapse;
+        width: 100%;
     }
 
-    .fill {
-        width: 168px;
-        height: 0px;
-        border: 1px solid #000;
-        background: green;
-        left: 4px;
-        border-radius: 11px;
-        position: absolute;
-        bottom: 37px;
+    #customers td, #customers th {
+        border: 1px solid #ddd;
+        padding: 8px;
     }
-    .percentage {
-        margin-left: 66px;
-        color: #fff;
+
+    #customers tr:nth-child(even){background-color: #f2f2f2;}
+
+    #customers tr:hover {background-color: #ddd;}
+
+    #customers th {
+        padding-top: 12px;
+        padding-bottom: 12px;
+        text-align: left;
+        background-color: #4CAF50;
+        color: white;
     }
 </style>
 
 @section('content')
     <div class="container">
-        <table class="table">
+        <table id="customers">
             <thead>
             <tr>
-                <th scope="col">Condition</th>
-                <th scope="col">Counter</th>
-                <th scope="col">Else Part Distance</th>
-                <th scope="col">Check Water Counter</th>
-                <th scope="col">Pump Mode Condition</th>
-                <th scope="col">Water Check Condition</th>
-                <th scope="col">master_status</th>
-                <th scope="col">Pump1 Pin Status</th>
-                <th scope="col">Pump On Condition</th>
-                <th scope="col">Master Control</th>
-                <th scope="col">Pump2 Pin Status</th>
-                <th scope="col">Pump Control Mode</th>
-                <th scope="col">Select Pump</th>
-                <th scope="col">Last Pump On</th>
-                <th scope="col">Pump Start Height</th>
-                <th scope="col">Low Level</th>
-                <th scope="col">High Level</th>
+                <th>Key</th>
+                <th>Value</th>
             </tr>
             </thead>
             <tbody>
-            <tr>
-                <td>{{ $data['Condition'] }}</td>
-                <td>{{ $data['Counter'] }}</td>
-                <td>{{ $data['else_part_distance'] }}</td>
-                <td>{{ $data['check_water_counter'] }}</td>
-                <td>{{ $data['pump_mode_condition'] }}</td>
-                <td>{{ $data['water_check_condition'] }}</td>
-                <td>{{ $data['master_status'] }}</td>
-                <td>{{ $data['Pump1 Pin Status'] }}</td>
-                <td>{{ $data['Pump On Condition'] }}</td>
-                <td>{{ $data['Master Control'] }}</td>
-                <td>{{ $data['Pump2 Pin Status'] }}</td>
-                <td>{{ $data['Pump Controll Mode'] }}</td>
-                <td>{{ $data['Select Pump'] }}</td>
-                <td>{{ $data['Last Pump On'] }}</td>
-                <td>{{ $data['Pump Start Height'] }}</td>
-                <td>{{ $data['Low Level'] }}</td>
-                <td>{{ $data['High Level'] }}</td>
-            </tr>
+            @foreach($data as $key => $value)
+                <tr>
+                    <td>{{ $key }}</td>
+                    <td>{{ $value }}</td>
+                </tr>
+            @endforeach
             </tbody>
         </table>
     </div>
